dcef450c3873090ca34d8ac2b41ba6d212bd212e
Craig Wm. Versek <cversek+github@gmail.com>
2013-03-06 19:31:50 -0500

    work around for libv42l "Cannot allocate memory" when using multiple webcams at high res

--- twocam_test.py
+++ twocam_test.py
@@ -1,15 +1,27 @@
 import time
 from SimpleCV import *
 
-RESOLUTION = (640,480)
-CAM0_PROPS = {'width':RESOLUTION[0],'height':RESOLUTION[1]}
-CAM1_PROPS = CAM0_PROPS.copy()
+#RESOLUTION = (640,480)
+#CAM0_PROPS = {'width':RESOLUTION[0],'height':RESOLUTION[1]}
+#CAM1_PROPS = CAM0_PROPS.copy()
 
-cam0 = Camera(0,CAM0_PROPS)
-cam1 = Camera(1,CAM1_PROPS)
+c0 = cv.CaptureFromCAM(0)
+cv.GrabFrame(c0)
+frame = cv.RetrieveFrame(c0)
+newimg = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_8U, 3)
+cv.Copy(frame, newimg)
+img0 = Image(newimg)
 
-img0 = cam0.getImage()
-img1 = cam1.getImage()
+#FIXME - delete the first capture buffer to prevent 
+# the error "libv4l2: error turning on stream: No space left on device"
+del(c0)
+
+c1 = cv.CaptureFromCAM(1)
+cv.GrabFrame(c1)
+frame = cv.RetrieveFrame(c1)
+newimg = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_8U, 3)
+cv.Copy(frame, newimg)
+img1 = Image(newimg)
 
 dual_img = img0.sideBySide(img1)
 dual_img.show()
